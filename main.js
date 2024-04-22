c9871049526f35a5bb7b72cb44cde3693a677f93
donaldsontu520 <153301884+donaldsontu520@users.noreply.github.com>
2024-04-22 10:51:29 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+// Tucker Donaldson
+// 19 APR 2024
+// JS Calculator
+
+// Input
+const firstName = 'Mike';
+const lastName = 'Donaldson';
+const num1 = 20;
+const num2 = 10;
+
+
+// Process
+// Addition
+const additionResult = num1 + num2;
+
+// Subtraction
+const subtractionResult = num1 - num2;
+// Multiplication
+const multiplcationResult = num1 * num2;
+
+// Division
+const divisionResult = num1 / num2;
+
+// Output
+
+
+const myOutput = `
+${firstName} ${lastName}
+
+${num1} + ${num2} = ${additionResult}
+${num1} - ${num2} = ${subtractionResult}
+${num1} * ${num2} = ${multiplcationResult}
+${num1} / ${num2} = ${divisionResult}
+`;
+
+console.log(myOutput);
